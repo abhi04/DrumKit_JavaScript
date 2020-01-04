5db3342b8f86109874940cd880162b9b17219a4a
abhi04 <vijarpit007@gmail.com>
2020-01-05 01:16:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+for(var i =0 ;  i<document.querySelectorAll(".drum").length ; i++){
+  document.querySelectorAll("button")[i].addEventListener("click",function () {
+    var btnInnerHtml = this.innerHTML;
+     swicthCase(btnInnerHtml);
+     buttonAmimation(btnInnerHtml);
+    // var audio = new Audio('sounds/tom-1.mp3');
+    // audio.play();
+  });
+}
+
+document.addEventListener("keydown",function(x){
+    swicthCase(x.key);
+    buttonAmimation(x.key);
+})
+
+function swicthCase(value){
+
+  switch (value) {
+    case 'w':
+       var audio = new Audio('sounds/crash.mp3');
+       audio.play();
+       break;
+    case 'a':
+        var audio = new Audio('sounds/kick-bass.mp3');
+        audio.play();
+        break;
+    case 's':
+       var audio = new Audio('sounds/snare.mp3');
+        audio.play();
+        break;
+    case 'd':
+    var audio = new Audio('sounds/tom-1.mp3');
+     audio.play();
+     break;
+    case 'j':
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+    case 'k':
+       var audio = new Audio('sounds/tom-3.mp3');
+       audio.play();
+       break;
+    case 'l':
+       var audio = new Audio('sounds/tom-4.mp3');
+       audio.play();
+       break;
+    default:
+
+  }
+
+
+}
+
+function buttonAmimation(value){
+
+  document.querySelector("." + value).classList.add("pressed");
+  setTimeout(function(){
+    document.querySelector("." + value).classList.remove("pressed");
+  },100)
+
+}
